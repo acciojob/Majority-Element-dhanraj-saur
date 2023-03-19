3c63208b33b5c5f7f6f428a8832b16ef56168a5b
dhanraj-saur <121377364+dhanraj-saur@users.noreply.github.com>
2023-03-19 17:44:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your code here
 function majorityElement() {
 	let max = 0, count = 0;
-	for(let i=0;i<num.lenght;i++){
-		if(count == 0) max = num[i];
-		if(num[i] == max) count++;
+	for(let i=0;i<nums.length;i++){
+		if(count == 0) max = nums[i];
+		if(nums[i] == max) count++;
 		else count--;
 	}
 	return max;
